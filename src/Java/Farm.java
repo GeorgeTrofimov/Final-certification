a176afd22d489c7b3d271db78cce7319581025c8
George <tro37295@gmail.com>
2023-04-30 22:20:44 +0500

    ready

---
+++ src/Java/Farm.java
@@ -0,0 +1,63 @@
+package Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Java.Beasts.Beast;
+import Java.Pets.Pet;
+
+public class Farm<animals> {
+    private static List<Animal> animals;
+
+    public Farm() {
+        animals = new ArrayList();
+    }
+
+    public boolean adopt(Animal animal) {
+        return animals.add(animal);
+    }
+
+    public Farm<Animal> release(Farm<Animal> animalFarm, Animal animal) {
+        if (animals.contains(animal)) {
+            animals.remove(animal);
+            animalFarm.adopt(animal);
+            return animalFarm;
+        }
+        return null;
+    }
+
+    public static <A> Farm<A> create() {
+        return new Farm<A>();
+    }
+
+    public static <A> boolean adopt(Farm<A> farm, Animal animal) {
+        return farm.adopt(animal);
+    }
+
+    public void printOnlyThisClass(int animalClass) {
+        switch (animalClass) {
+            case 1:
+                printCollection(Pet.class);
+                break;
+            case 2:
+                printCollection(Beast.class);
+                break;
+        }
+    }
+
+    public void printCollection(Class<?> choiseClass){
+        for (Animal animal :
+                animals) {
+            if (choiseClass.isInstance(animal)) {
+                System.out.println(animal.toString());
+            }
+        }
+    }
+
+    public static void printAnimals() {
+        for (Animal animal :
+                animals) {
+            System.out.println(animal.toString());
+        }
+    }
+}
